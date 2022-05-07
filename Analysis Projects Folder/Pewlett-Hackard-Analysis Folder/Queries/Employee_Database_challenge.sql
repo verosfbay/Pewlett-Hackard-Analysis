f5d35cf0ad995f3545646f47b613b5c9321586f0
Veronica Lobkina <vlobkina@outlook.com>
2022-05-07 16:31:58 -0700

    deliverable 2

--- Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/Employee_Database_challenge.sql
+++ Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/Employee_Database_challenge.sql
@@ -33,3 +33,22 @@ into retiring_titles
 from unique_titles 
 group by title
 order by count(title) desc ;
+
+-- Deliverable 2: The Employees Eligible for the Mentorship Program
+select distinct on (e.emp_no)
+	e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+into mentorship_eligibility 
+from employees as e 
+inner join dept_employees as de
+	on e.emp_no = de.emp_no
+inner join titles as t 
+on e.emp_no = t.emp_no
+where de.to_date = '9999-01-01'
+	and (e.birth_date between '1965-01-01' and '1965-12-31')
+order by e.emp_no; 
