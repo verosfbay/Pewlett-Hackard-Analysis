395b660f8501b4e27f0e96ed3bffb26728d657c7
Veronica Lobkina <vlobkina@outlook.com>
2022-05-06 19:48:55 -0700

    sql stuff

---
+++ EmployeeDBText.sql
@@ -0,0 +1,42 @@
+Departments
+-
+dept_no varchar pk
+dept_name varchar
+
+Department_Employees
+-
+emp_no int pk fk - Employees.emp_no
+dept_no varchar pk fk - Departments.dept_no
+from_date date
+to_date date
+
+Department_Managers
+-
+dept_no varchar pk fk - Departments.dept_no
+emp_no int pk fk - Employees.emp_no
+from_date date 
+to_date date
+
+Employees
+-
+emp_no int pk
+birth_date date
+first_name varchar
+last_name varchar
+gender varchar 
+hire_date date
+
+Salaries
+- 
+emp_no int pk fk - Employees.emp_no
+salary int
+from_date date
+to_date date
+
+Titles
+-
+emp_no int pk fk - Employees.emp_no
+title varchar
+from_date date
+to_date date
+
---
+++ schema.sql
@@ -0,0 +1,57 @@
+-- Creating tables for PH-EmployeesDB
+create table departments (
+	dept_no varchar(4) not null,
+	dept_name varchar(40) not null,
+	primary key (dept_no),
+	unique (dept_name)
+);
+
+create table employees (
+	emp_no int not null,
+	birth_date date not null,
+	first_name varchar not null,
+	last_name varchar not null,
+	gender varchar not null,
+	hire_date varchar not null,
+	primary key (emp_no)
+); 
+
+CREATE TABLE dept_manager (
+dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+
+create table salaries (
+	emp_no int not null,
+	salary int not null,
+	from_date date not null,
+	to_date date not null,
+foreign key (emp_no) references employees (emp_no),
+primary key (emp_no)
+); 
+
+create table dept_employees (
+	emp_no int not null,
+	dept_no varchar not null,
+	from_date date not null,
+	to_date date not null,
+foreign key (emp_no) references employees (emp_no),
+foreign key (dept_no) references departments (dept_no),
+primary key (emp_no)
+);
+
+create table titles (
+	emp_no int not null,
+	title varchar not null,
+	from_date date not null,
+	to_date date not null,
+foreign key (emp_no) references employees (emp_no),
+primary key (emp_no)
+);
+
+select * from departments;
