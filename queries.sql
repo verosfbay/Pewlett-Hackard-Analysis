fab98a7953b668664f82886a80e5aef91fa0e63f
Veronica Lobkina <vlobkina@outlook.com>
2022-05-07 10:14:41 -0700

    retirement eligibility

--- queries.sql
+++ queries.sql
@@ -7,3 +7,22 @@ where birth_date between '1952-01-01' and '1955-12-31';
 select first_name, last_name
 from employees
 where birth_date between '1952-01-01' and '1952-12-31';
+
+-- Create three more queries to search for employees who were born in 1953, 1954, and 1955.
+select first_name, last_name
+from employees
+where birth_date between '1953-01-01' and '1953-12-31'; 
+
+-- Retirement eligibility
+select first_name, last_name
+from employees
+where (birth_date between '1952-01-01' and '1955-12-31')
+and (hire_date between '1985-01-01' and '1988-12-31');
+
+
+-- Retirement eligibility updated
+select first_name, last_name 
+into retirement_info
+from employees
+where (birth_date between '1952-01-01' and '1955-12-31')
+and (hire_date between '1985-01-01' and '1988-12-31');
