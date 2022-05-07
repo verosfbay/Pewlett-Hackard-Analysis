333a09d0617228a6cf30960febe9d1dd1d28a6d5
Veronica Lobkina <vlobkina@outlook.com>
2022-05-07 11:08:41 -0700

    queries

--- queries.sql
+++ queries.sql
@@ -115,3 +115,53 @@ on ce.emp_no = de.emp_no
 group by de.dept_no 
 order by de.dept_no;
 
+select * from employees_by_dept; 
+
+-- Employee Information: A list of employees containing 
+-- their unique employee number, their last name, first name, gender, and salary
+
+select e.emp_no, 
+	e.first_name,
+ 	e.last_name,
+	e.gender,
+	s.salary,
+	de.to_date
+into emp_info 
+from employees as e
+inner join salaries as s
+on (e.emp_no = s.emp_no)
+inner join dept_employees as de
+on (e.emp_no = de.emp_no)
+ 	where (e.birth_date between '1952-01-01' and '1955-12-31') -- update with aliases
+ 	and (e.hire_date between '1985-01-01' and '1988-12-31')
+ 	and (de.to_date = '9999-01-01'); 
+
+-- list of managers per department 
+select dm.dept_no,
+		d.dept_name,
+		dm.emp_no,
+		ce.last_name,
+		ce.first_name,
+		dm.from_date,
+		dm.to_date
+into manager_info
+from dept_manager as dm
+	inner join departments as d
+		on (dm.dept_no = d.dept_no)
+	inner join current_emp as ce
+		on (dm.emp_no = ce.emp_no); 
+
+-- The final list needs only to have the departments added to the current_emp table
+
+select ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+into dept_info
+from current_emp as ce
+inner join dept_employees as de
+on (ce.emp_no = de.emp_no)
+inner join departments as d
+on (de.dept_no = d.dept_no); 
+
+select * from dept_info ; 
