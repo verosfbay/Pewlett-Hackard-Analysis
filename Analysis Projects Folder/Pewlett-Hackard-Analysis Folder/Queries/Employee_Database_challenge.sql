b93d0f0efaf134c0d17996076fec6d1b48b37a90
Veronica Lobkina <vlobkina@outlook.com>
2022-05-07 13:15:22 -0700

    unique titles

--- Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/Employee_Database_challenge.sql
+++ Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/Employee_Database_challenge.sql
@@ -12,3 +12,12 @@ inner join titles as t
 on e.emp_no = t.emp_no
 where (e.birth_date between '1952-01-01' and '1955-12-31')
 order by e.emp_no ; 
+
+SELECT DISTINCT ON (rt.emp_no)
+rt.emp_no, 
+rt.first_name,
+rt.last_name,
+rt.title
+INTO unique_titles
+FROM retirement_titles as rt 
+ORDER BY emp_no, to_date DESC;	
